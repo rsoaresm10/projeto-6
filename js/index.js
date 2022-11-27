8cd3141c4889e4897036b34397cecc008b87691c
rafael <rafa100103soaresmendes@gmail.com>
2022-11-26 21:39:11 -0300

    create router.js

--- js/index.js
+++ js/index.js
@@ -4,23 +4,7 @@ const routes= {
     "/contact": "/pages/contact.html",
     404: "/pages/404.html"
 }
-function route(event) {
-event = event || window.event
-event.preventDefault()
-window.history.pushState({}, "", event.target.href )
-handle()
-}
 
-function handle (){
-    const {pathname }= window.location
-    const route = routes[pathname] || routes[404]
-    console.log('antes do fetch')
-    fetch(route)
-    .then(data => data.text())
-    .then(html => {document.querySelector('#app').innerHTML = html})
-    console.log(route)
-
-}
 handle()
 
 window.onpopstate = () => handle()
